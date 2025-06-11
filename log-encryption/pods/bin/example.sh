db8d274e79d54092b2c70c32f37016e2787af4db
Sashwat K <Sashwat.K@ibm.com>
2025-06-11 23:17:48 +0530

    fix: messages in examples

--- log-encryption/pods/bin/example.sh
+++ log-encryption/pods/bin/example.sh
@@ -5,5 +5,5 @@ apt-get update
 apt-get install -y openssl
 
 echo "To be, or not to be, that is the question:"
-echo "This message is generated from Hyper Protect Virtual Server (HPVS) through Docker Compose Contract." \
+echo "This message is generated from Podman Play Contract." \
     | /bin/bash /var/logging/bin/encrypt-log.sh /var/logging/logging.pub
