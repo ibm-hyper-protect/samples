d615adc0228b1e14a31525c10f14b4f1f403897f
Sashwat K <Sashwat.K@ibm.com>
2026-01-22 22:28:05 +0530

    feat: add loop for encryption

--- log-encryption/pods/bin/example.sh
+++ log-encryption/pods/bin/example.sh
@@ -4,6 +4,13 @@ set -eu
 apt-get update
 apt-get install -y openssl
 
-echo "To be, or not to be, that is the question:"
-echo "This message is generated from Podman Play Contract." \
-    | /bin/bash /var/logging/bin/encrypt-log.sh /var/logging/logging.pub
+# Run infinite loop
+while true; do
+    date | while IFS= read -r line; do
+        # Print to screen
+        echo "Screen: $line"
+        # Send to your encrypt script
+        echo "$line" | /bin/bash /var/logging/bin/encrypt-log.sh /var/logging/logging.pub
+    done
+    sleep 1
+done
